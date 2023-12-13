b60a5aefd92afa7e05ce49b926d330476a135500
Aeon Tanvir <aeontanvir@gmail.com>
2023-12-12 22:42:49 -0600

    Spring: Mapper config

---
+++ spring-blogs/src/main/java/com/aeontanvir/springblogs/config/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package com.aeontanvir.springblogs.config;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
