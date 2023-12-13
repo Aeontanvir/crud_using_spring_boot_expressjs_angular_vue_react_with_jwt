1cee725c4ce264618423510f06f42764c4614496
Aeon Tanvir <aeontanvir@gmail.com>
2023-12-12 20:39:05 -0600

    Spring boot project init

---
+++ spring-blogs/src/main/java/com/aeontanvir/springblogs/SpringBlogsApplication.java
@@ -0,0 +1,13 @@
+package com.aeontanvir.springblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogsApplication.class, args);
+	}
+
+}
