67b0367d1b05241aceb448520bd3c4f5ce0164a3
Aeon Tanvir <aeontanvir@gmail.com>
2023-12-13 12:59:59 -0600

    Spring: Post controller impl

--- spring-blogs/src/main/java/com/aeontanvir/springblogs/controllers/PostController.java
+++ spring-blogs/src/main/java/com/aeontanvir/springblogs/controllers/PostController.java
@@ -5,9 +5,7 @@ import com.aeontanvir.springblogs.responses.ApiResponse;
 import com.aeontanvir.springblogs.services.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,9 +15,30 @@ import java.util.List;
 public class PostController {
     private final PostService postService;
 
-    @GetMapping("")
+    @GetMapping
     public ResponseEntity<ApiResponse<List<PostDto>>> getAll() {
         return ResponseEntity.ok(ApiResponse.success("All Post List", postService.getAll()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<PostDto>> getById(@PathVariable Long id) throws Exception {
+        return ResponseEntity.ok(ApiResponse.success("Post Details", postService.getById(id)));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<PostDto>> create(@RequestBody PostDto postDto) throws Exception {
+        return ResponseEntity.ok(ApiResponse.success("Post Created", postService.create(postDto)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<PostDto>> update(@RequestBody PostDto postDto, @PathVariable Long id) throws Exception {
+        return ResponseEntity.ok(ApiResponse.success("Post Updated", postService.update(postDto, id)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<String>> delete(@PathVariable Long id) throws Exception {
+        postService.delete(id);
+        return ResponseEntity.ok(ApiResponse.success("Post Deleted", null));
     }
 
 }
