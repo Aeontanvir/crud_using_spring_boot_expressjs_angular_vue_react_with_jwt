01e6f6bd6ffa443742cef8736f90d7265d30dcec
Aeon Tanvir <aeontanvir@gmail.com>
2023-12-15 22:24:24 -0600

    React: Blog display

--- react-blogs/src/App.js
+++ react-blogs/src/App.js
@@ -2,14 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Blogs from './pages/Blogs';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="max-w-screen-xl justify-between mx-auto p-4">
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/blogs' element={<Blogs />} />
         </Routes>
       </div>
     </div>
---
+++ react-blogs/src/pages/Blogs.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import FeatureBlog from '../components/FeatureBlog'
+import axios from 'axios';
+
+export default function Blogs() {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/v1/posts").then(resp => {
+            setBlogs(resp.data.data);
+        })
+    }, []);
+    return (
+
+
+        <section className="bg-white dark:bg-gray-900">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+                    <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
+                    <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
+                </div>
+                <div className="grid gap-8 lg:grid-cols-2">
+                    {
+                        blogs.map(blog => {
+                            return <FeatureBlog {...blog} />
+                        })
+                    }
+                </div>
+            </div>
+        </section>
+    )
+}
