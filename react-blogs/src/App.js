d5c17c57c6b6928f2b03d0a34aa00f02f9d61845
Aeon Tanvir <aeontanvir@gmail.com>
2023-12-15 18:39:24 -0600

    React Basic Tailwind css setup

--- react-blogs/src/App.js
+++ react-blogs/src/App.js
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar />
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
---
+++ react-blogs/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='w-full h-full flex flex-col'>
+        <h1>Home Page</h1>
+    </div>
+  )
+}
