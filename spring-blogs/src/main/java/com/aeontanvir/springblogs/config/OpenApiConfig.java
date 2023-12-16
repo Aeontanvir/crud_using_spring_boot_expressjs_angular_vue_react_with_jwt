b7d9178e6d5fdaab0961ef5dbf178917c52ce1d6
Aeon Tanvir <aeontanvir@gmail.com>
2023-12-15 21:52:00 -0600

    Spring: Swagger Api added

---
+++ spring-blogs/src/main/java/com/aeontanvir/springblogs/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.aeontanvir.springblogs.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Spring Blogs", version = "1.0.0",
+        description = "REST API description...",
+        contact = @Contact(name = "Aeon Tanvir", email="aeontanvir@gmail.com")),
+        security = {@SecurityRequirement(name = "bearerToken")}
+)
+@SecuritySchemes({
+        @SecurityScheme(name = "bearerToken", type = SecuritySchemeType.HTTP,
+                scheme = "bearer", bearerFormat = "JWT")
+})
+public class OpenApiConfig {
+}
--- spring-blogs/src/main/java/com/aeontanvir/springblogs/initdata/BootDataImporter.java
+++ spring-blogs/src/main/java/com/aeontanvir/springblogs/initdata/BootDataImporter.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
@@ -19,20 +21,29 @@ public class BootDataImporter implements CommandLineRunner {
     private final PostRepository postRepository;
     @Override
     public void run(String... args) throws Exception {
+        List<String> wordList = Arrays.asList("apple", "banana", "orange", "grape", "kiwi", "melon", "peach", "pear");
         List<Post> posts = IntStream.rangeClosed(1, 10)
-                .mapToObj(num-> new Post(num+"Title", num+"Details"))
-//                .map(post->{
-//                    post.setComments(
-//                            IntStream.rangeClosed(1, 10)
-//                                    .mapToObj(num-> new Comment(num+"Comment", post ))
-//                                    .toList()
-//                    );
-//                    return post;
-//
-//                })
-                .toList();
+                .mapToObj(num -> new Post(num + generateRandomWordString(wordList, 5), num + generateRandomWordString(wordList, 40)))
+                .map(post -> {
+                    post.setComments(
+                            IntStream.rangeClosed(1, 10)
+                                    .mapToObj(num -> new Comment(num + generateRandomWordString(wordList, 10), post))
+                                    .collect(Collectors.toList())
+                    );
+                    return post;
+                })
+                .collect(Collectors.toList());
+
         postRepository.saveAll(posts);
 
+    }
+
+    private static String generateRandomWordString(List<String> wordList, int numberOfWords) {
+        Random random = new Random();
+
+        return random.ints(numberOfWords, 0, wordList.size())
+                .mapToObj(wordList::get)
+                .collect(Collectors.joining(" "));
     }
 
 }
